7611967ffe1e2575a13732d8336443ceeea2d62e
Henry Le <henrysama58@gmail.com>
2022-02-03 23:38:11 -0500

    redesigned portfolio with javascript functionalities

---
+++ index.js
@@ -0,0 +1,54 @@
+let isModalOpen = false;
+let contrast = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+  
+    for (let i = 0; i < shapes.length; i++) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+    }
+  }
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal-overlay-loading');
+    const success = document.querySelector('.modal-overlay-success');
+
+    loading.classList += ' modal-overlay-visible'
+     emailjs
+        .sendForm (
+            'service_rh0kavb', 'template_7fa317p', event.target, 'user_DRxJ18uk3xgkYakVhvZLw'
+    ).then(() => {
+        console.log('this worked');
+        loading.classList.remove('modal-overlay-visible');
+        success.classList += ' modal-overlay-visible';
+    }).catch(() => {
+        loading.classList.remove('modal-overlay-visible');
+        alert('The email service is temporarily unavailable. Please contact me directly at henrysama58@gmail.com.');
+    }
+    )
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove('modal-open');
+    }
+    isModalOpen = true;
+    document.body.classList += ' modal-open';
+}
+
+function toggleContrast() {
+    contrast = !contrast;
+    if (contrast) {
+        document.body.classList += ' dark-mode';
+    } else {
+    document.body.classList.remove('dark-mode');
+    }
+}
